6cab5ededb2fb5f0c2ef9c791afee730d1cd7f2c
banana-piecake <pea0019@balwynhs.vic.edu.au>
2026-09-13 11:10:42 +1000

    holding jump whilst in air adds velocity, meaning you can now jump higher by holding

--- gorbformer/player.gd
+++ gorbformer/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
+const AIR_FLOAT_VELOCITY = -10.0
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +14,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# air float thingy.
+	if Input.is_action_pressed("jump") and not is_on_floor():
+		velocity.y = velocity.y + AIR_FLOAT_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
