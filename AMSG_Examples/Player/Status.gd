483a3b99f61121c13a0e8b508b0987ea94dee573
banana-piecake <pea0019@balwynhs.vic.edu.au>
2026-09-13 10:09:54 +1000

    work please

---
+++ AMSG_Examples/Player/Status.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var camera_root : CameraComponent
+@onready var player = get_parent()
+@onready var direction = $Control/Direction
+@onready var velocity = $Control/Velocity
+@onready var mesh = $Control/mesh
+
+func _physics_process(_delta):
+	visible = camera_root.Camera.current
+	var h_rot = get_parent().get_node("SpringArm3D").transform.basis.get_euler().y
+	var character_node_velocity = player.velocity if player is CharacterBody3D else player.linear_velocity
+	$Control.set_rotation(h_rot)
+#	direction.rotation = atan2(player.direction.z, player.direction.x) 
+	velocity.position = Vector2(character_node_velocity.x, character_node_velocity.z) * 10 
+	mesh.rotation = 90-get_node("../Armature").rotation.y - player.rotation.y - .5
---
+++ first project/Player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const UP = Vector2(0,-1)
+const FLAP = 200
+const MAXFALLSPEED = 200
+const GRAVITY = 10
+
+var motion = Vector2()
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
